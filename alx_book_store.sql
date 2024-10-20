58905bdddfe4f2707e4c24dab26f61714eb77997
nezhanour <nezha95nour@gmail.com>
2024-10-20 14:04:39 +0100

    Modified SQL keywords to uppercase alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-create table Books (
+CREATE TABLE Books (
 book_id INT Primary Key,
 title VARCHAR(130),
 Foreign Key (author_id) references Authors(author_id),
@@ -8,25 +8,25 @@ price DOUBLE,
 publication_date DATE
 );
 
-create table Authors (
+CREATE TABLE Authors (
 author_id INT Primary Key,
 author_name VARCHAR(215)
 );
 
-create table Customers (
+CREATE TABLE Customers (
 customer_id INT Primary Key,
 customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT
 );
 
-create table Orders (
+CREATE TABLE Orders (
 order_id INT Primary Key,
 Foreign Key (customer_id) references Customers(customer_id),
 order_date DATE
 ); 
 
-create table Order_Details(
+CREATE TABLE Order_Details(
 orderdetailid INT Primary Key,
 Foreign Key (order_id) references Orders(order_id),
 Foreign Key (book_id) references Books(book_id),
